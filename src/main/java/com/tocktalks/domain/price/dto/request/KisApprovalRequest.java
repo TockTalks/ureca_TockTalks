d3f5247468631bac702fc63b852820ab2122e144
JAEUNGCHOI <jaeung0129@gmail.com>
2026-07-15 14:18:20 +0900

    feat: KIS 인증 API 요청/응답 DTO 추가

--- src/main/java/com/tocktalks/domain/price/dto/request/KisApprovalRequest.java
+++ src/main/java/com/tocktalks/domain/price/dto/request/KisApprovalRequest.java
@@ -1,4 +1,13 @@
 package com.tocktalks.domain.price.dto.request;
 
-public class KisApprovalRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KisApprovalRequest (
+        @JsonProperty("grant_type") String grantType,
+        String appkey,
+        String secretkey
+) {
+    public static KisApprovalRequest of(String appKey, String appSecret){
+        return new KisApprovalRequest("client_credentials", appKey, appSecret);
+    }
 }
--- src/main/java/com/tocktalks/domain/price/dto/response/KisApprovalResponse.java
+++ src/main/java/com/tocktalks/domain/price/dto/response/KisApprovalResponse.java
@@ -1,4 +1,10 @@
 package com.tocktalks.domain.price.dto.response;
 
-public class KisApprovalResponse {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KisApprovalResponse (
+        @JsonProperty("approval_key") String approvalKey
+) {
 }
