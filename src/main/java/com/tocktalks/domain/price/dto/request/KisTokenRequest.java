4689226a0d4e2d925bb88e618209d541f0cc4c92
JAEUNGCHOI <jaeung0129@gmail.com>
2026-07-15 14:18:20 +0900

    feat: KIS 인증 API 요청/응답 DTO 추가

--- src/main/java/com/tocktalks/domain/price/dto/request/KisTokenRequest.java
+++ src/main/java/com/tocktalks/domain/price/dto/request/KisTokenRequest.java
@@ -1,4 +1,13 @@
 package com.tocktalks.domain.price.dto.request;
 
-public class KisTokenRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KisTokenRequest (
+        @JsonProperty("grant_type") String grantType,
+        String appkey,
+        String appsecret
+) {
+    public static KisTokenRequest of(String appKey, String appSecret) {
+        return new KisTokenRequest("client_credentials", appKey, appSecret);
+    }
 }
