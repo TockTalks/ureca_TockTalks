d02b866b7cff3db550ed1aaa91d6c4a1bbd7cdac
JAEUNGCHOI <jaeung0129@gmail.com>
2026-07-16 17:07:52 +0900

    feat: 종목별 price:{StockCode} 채널로 시세 발행 및 구독한 메시지를 STOMP로 전달

---
+++ src/main/java/com/tocktalks/domain/price/service/PricePublisher.java
@@ -0,0 +1,18 @@
+package com.tocktalks.domain.price.service;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PricePublisher {
+
+    private static final String CHANNEL_PREFIX = "price";
+    private final StringRedisTemplate redisTemplate;
+
+    public PricePublisher(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+    public void publish(String stockCode, String currentPrice) {
+        redisTemplate.convertAndSend(CHANNEL_PREFIX + stockCode, currentPrice);
+    }
+}
