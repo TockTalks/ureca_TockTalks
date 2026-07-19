c770dda3d22f4b12c1c26adbfd3664c4c0f17722
jeahong <burindol3@naver.com>
2026-07-20 00:30:37 +0900

    feat: 댓글 및 댓글 좋아요 레포지토리 작성

---
+++ src/main/java/com/tocktalks/domain/community/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.tocktalks.domain.community.repository;
+
+import com.tocktalks.domain.community.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByPostIdOrderByCreatedAyAsc(Long postId, Pageable pageable);
+
+    long countByPostId(Long postId);
+}
