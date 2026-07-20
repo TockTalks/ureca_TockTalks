0bf56d3ce8715aa74dd5fe60c81e21821b0e2c32
jeahong <burindol3@naver.com>
2026-07-20 13:40:49 +0900

    feat: admin 도메인 틀 작성

---
+++ src/main/java/com/tocktalks/domain/admin/repository/NoticeRepository.java
@@ -0,0 +1,10 @@
+package com.tocktalks.domain.admin.repository;
+
+import com.tocktalks.domain.admin.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Page<Notice> findAllByOrderByCreatedAtDesc(Pageable pageable);
+}
