521072f4c0b991785026a366b5069a57a6556897
burindol3 <burindol3@naver.com>
2026-07-20 11:39:38 +0900

    fix: 테스트

--- src/main/java/com/tocktalks/domain/ranking/repository/RoomRankingArchiveRepository.java
+++ src/main/java/com/tocktalks/domain/ranking/repository/RoomRankingArchiveRepository.java
@@ -4,7 +4,7 @@ import com.tocktalks.domain.ranking.entity.RoomRankingArchive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRankingArchiveRepository extends JpaRepository<RoomRankingArchive, Long> {
-
+    //테스트
     List<RoomRankingArchive> findByRoomIdOrderByFinalRankAsc(Long roomId);
 
     List<RoomRankingArchive> findByRoomIdOrderByFinalAssetDesc(Long roomId);
