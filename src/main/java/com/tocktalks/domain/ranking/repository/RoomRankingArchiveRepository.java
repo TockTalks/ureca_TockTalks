7bf30e63690cd29b0852220309c8665b0001cb22
burindol3 <burindol3@naver.com>
2026-07-20 13:22:21 +0900

    feat: import문 추가

--- src/main/java/com/tocktalks/domain/ranking/repository/RoomRankingArchiveRepository.java
+++ src/main/java/com/tocktalks/domain/ranking/repository/RoomRankingArchiveRepository.java
@@ -3,6 +3,9 @@ package com.tocktalks.domain.ranking.repository;
 import com.tocktalks.domain.ranking.entity.RoomRankingArchive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoomRankingArchiveRepository extends JpaRepository<RoomRankingArchive, Long> {
     //테스트
     List<RoomRankingArchive> findByRoomIdOrderByFinalRankAsc(Long roomId);
